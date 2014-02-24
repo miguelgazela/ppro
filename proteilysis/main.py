ca3be5ab8fd651e65b9b8822a509350527cbdc31
miguelgazela <miguel.gazela@gmail.com>
2014-02-24 19:09:35 +0000

    need to change from django to PHP

--- proteilysis/main.py
+++
@@ -1,8 +0,0 @@
-from PDBParser import PDBParser, fetch_pdb_file
-import json
-
-content = fetch_pdb_file('3GFT')
-pdb = PDBParser(content=content)
-
-with open('results.json', 'w') as fout:
-        fout.write(json.dumps(pdb.sequence, indent=1))
