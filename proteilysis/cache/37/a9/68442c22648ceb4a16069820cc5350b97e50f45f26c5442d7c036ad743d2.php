f3016497ff5267aa6121eba90d73bd4d8e527588
miguelgazela <miguel.gazela@gmail.com>
2014-03-03 14:02:14 +0000

    Added first bootstraped template page

--- proteilysis/cache/37/a9/68442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad743d2.php
+++ proteilysis/cache/37/a9/68442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad743d2.php
@@ -17,17 +17,75 @@ class __TwigTemplate_37a968442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad
     {
         // line 1
         echo "<!DOCTYPE html>
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <h1>";
-        // line 7
-        echo twig_escape_filter($this->env, ((array_key_exists("name", $context)) ? (_twig_default_filter((isset($context["name"]) ? $context["name"] : null), "User")) : ("User")), "html", null, true);
-        echo ", welcome to Proteilysis!</h1>
-    </body>
-</html>";
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+    <link rel=\"shortcut icon\" href=\"../../assets/ico/favicon.ico\">
+
+    <title>Proteilysis - Index</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href=\"static/css/bootstrap.min.css\" rel=\"stylesheet\">
+    <link href=\"static/css/global.css\" rel=\"stylesheet\">
+
+  </head>
+
+  <body>
+
+    <div class=\"container\">
+      <div class=\"header\">
+        <ul class=\"nav nav-pills pull-right\">
+          <li class=\"active\"><a href=\"#\">Home</a></li>
+          <li><a href=\"#\">About</a></li>
+          <li><a href=\"#\">Contact</a></li>
+        </ul>
+        <h3 class=\"text-muted\">Project name</h3>
+      </div>
+
+      <div class=\"jumbotron\">
+        <h1>Welcome to Proteilysis!</h1>
+        <p class=\"lead\">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class=\"btn btn-lg btn-success\" href=\"#\" role=\"button\">Sign up today</a></p>
+      </div>
+
+      <div class=\"row marketing\">
+        <div class=\"col-lg-6\">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class=\"col-lg-6\">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <div class=\"footer\">
+        <p>&copy; PPRO 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+  </body>
+</html>
+";
     }
 
     public function getTemplateName()
@@ -35,13 +93,8 @@ class __TwigTemplate_37a968442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad
         return "index.html";
     }
 
-    public function isTraitable()
-    {
-        return false;
-    }
-
     public function getDebugInfo()
     {
-        return array (  27 => 7,  19 => 1,);
+        return array (  19 => 1,);
     }
 }
