e6fafb1549b89936bf916f8a79a7b05fafc2df4b
miguelgazela <miguel.gazela@gmail.com>
2014-03-04 11:01:42 +0000

    Made some changes to the css of the main page

--- proteilysis/cache/37/a9/68442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad743d2.php
+++ proteilysis/cache/37/a9/68442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad743d2.php
@@ -43,13 +43,13 @@ class __TwigTemplate_37a968442c22648ceb4a16069820cc5350b97e50f45f26c5442d7c036ad
           <li><a href=\"#\">About</a></li>
           <li><a href=\"#\">Contact</a></li>
         </ul>
-        <h3 class=\"text-muted\">Project name</h3>
+        <h3 class=\"text-muted\">Proteilysis</h3>
       </div>
 
       <div class=\"jumbotron\">
         <h1>Welcome to Proteilysis!</h1>
         <p class=\"lead\">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class=\"btn btn-lg btn-success\" href=\"#\" role=\"button\">Sign up today</a></p>
+        <p><a class=\"btn btn-success\" href=\"#\" role=\"button\">Sign up today</a></p>
       </div>
 
       <div class=\"row marketing\">
