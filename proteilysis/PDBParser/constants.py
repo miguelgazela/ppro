c29457bd256540a2abeb2645ae58e74e0f055d61
miguelgazela <miguel.gazela@gmail.com>
2014-03-13 23:40:58 +0000

    added function to execute the mapping from PDB ID to UniProt ID

--- proteilysis/PDBParser/constants.py
+++ proteilysis/PDBParser/constants.py
@@ -2,3 +2,5 @@
 
 BASE_URL = "http://www.rcsb.org/pdb/files"
 NO_SUCH_FILE_URL = "http://www.rcsb.org/pdb/nosuchfile.jsp"
+
+ID_MAPPING_URL = "http://www.uniprot.org/mapping/"
--- proteilysis/PDBParser/utils.py
+++ proteilysis/PDBParser/utils.py
@@ -2,15 +2,15 @@
 
 import requests
 import re
-from constants import BASE_URL, NO_SUCH_FILE_URL
+import constants as const
 
 def fetch_pdb_file(id):
     """
     Returns the content of a pdb file if it exists or None if it doesn't
     """
-    content = requests.get("%s/%s.pdb" % (BASE_URL, id))
+    content = requests.get("%s/%s.pdb" % (const.BASE_URL, id))
     
-    if not content.url == NO_SUCH_FILE_URL:
+    if not content.url == const.NO_SUCH_FILE_URL:
         return content.text
     return None
 
@@ -29,6 +29,16 @@ def parse_pisces_file(filename):
                         for line in lines if re.search('^[A-Z0-9]{5}', line)]
     except IOError:
         return None
+
+
+def idmapping(id):
+    """
+    Does the mapping of a pdb id to a UniProtKB id and returns it
+    """
+    content = requests.get(const.ID_MAPPING_URL,
+        params={'from': 'PDB_ID', 'to': 'ACC', 'format': 'tab', 'query': id})
+    
+    return content.text
 
 
     
