780d98187524c41b52287cd986ea1f152b8c3596
miguelgazela <miguel.gazela@gmail.com>
2014-03-17 18:45:57 +0000

    started adding database functions

---
+++ proteilysis/Database/protein.php
@@ -0,0 +1,6 @@
+<?php
+    include_once('../Common/init.php');
+    include_once($BASE_PATH . 'Common/DatabaseException.php');
+
+    
+?>
--- proteilysis/index.php
+++ proteilysis/index.php
@@ -1,6 +1,8 @@
 <?php
     ini_set('display_errors', 'On');
 
+    require_once('Database/protein.php');
+
     require 'lib/Slim/Slim.php';
     \Slim\Slim::registerAutoloader();
 
@@ -26,6 +28,7 @@
     });
 
     $app->get('/proteins', function () use ($app) {
+
         $app->render('proteins/list.html');
     });
 
